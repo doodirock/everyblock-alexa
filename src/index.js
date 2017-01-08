c7bdbd9842321e659a4dd17ecdca8462e1ce25e2
Joel Albert <jalbert@highschoolcube.com>
2017-01-07 23:16:48 -0600

    axios calls

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const _ = require('lodash');
 const alexa = require('alexa-app');
 const axios = require("axios");
-const conf = require('./data/config.json');
+const conf = require('./data/config.json'); // Used to store secret tokens and other things you want to hide
 
 const ax = axios.create({
   baseURL: 'https://api.everyblock.com/',
-  headers: {'Authorization': conf.secret.Auth} // you will need to add your own config file and token here
+  headers: {'Authorization': conf.secret.Auth} // you will need to add your own secret token here
 });
 
 const app = new alexa.app();
@@ -16,9 +16,18 @@ app.launch((request, response) => {
 });
 
 app.intent("GetEvents", (request, response) => {
+    
     var city = request.slot('City');
-    var txt = 'It seems like the city of '+city+' is going to have a lovely weekend';
-    return response.say(txt);
+    var txt = '';
+
+    ax.get('content/'+city+'/topnews/.json')
+      .then((data) => {
+         response.say('Hey it worked!');
+      })
+      .catch((error) => {
+        response.say('Sorry there seems to be an issue');
+      });  
+    return false;
   }
 );
 

bd135f51ea816656e6a48524c12fd245ba52d052
Joel Albert <jalbert@highschoolcube.com>
2017-01-07 20:44:48 -0600

    first push

---
+++ README.md
@@ -0,0 +1,21 @@
+# Random Alexa App that does cool stuff
+
+![picture alt](https://cdn.meme.am/cache/instances/folder384/55167384.jpg "Alexa everywhere")
+
+- - - -
+
+To install simply **npm i** and away you go!  This package contains gulp, however it is only used to deploy to lambda.
+
+## What I use
+
+* AWS Lamda
+* AWS Alexa
+* ES6
+* Node.js 4
+* Bluebird for promises
+* Gulp to deploy
+
+## Libraries
+* [alexa-app](https://github.com/matt-kruse/alexa-app)
+* [bluebird](http://bluebirdjs.com/docs/getting-started.html)
+* [lodash](https://lodash.com/)
---
+++ src/index.js
@@ -0,0 +1,23 @@
+const _ = require('lodash');
+const alexa = require('alexa-app');
+const Blue = require("bluebird");
+
+const app = new alexa.app();
+app.launch((request, response) => {
+  response.say("Alexa hears you loud and clear").send();
+  // if this is an async handler you must return false
+  //return false;
+});
+
+app.intent("WhoAreWe", (request, response) => {
+    response.say("Chicago streets for days.");
+  }
+);
+
+app.intent("WhatsUp", (request, response) => {
+    response.say("Whats going on in your hood is amazing.");
+  }
+);
+
+// connect to lambda
+exports.handler = app.lambda();

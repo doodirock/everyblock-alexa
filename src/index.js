a619070a3d6cd7975b108743a11ea8e457320b1a
Joel Albert <jalbert@highschoolcube.com>
2017-01-08 18:52:47 -0600

    a little more info for help

--- src/index.js
+++ src/index.js
@@ -62,8 +62,8 @@ app.intent("GetEvents", (request, response) => {
 
 app.intent("AMAZON.HelpIntent", (request, response) => {
     response.say('<p>You can request reports from several different topics in your neighborhood.</p>'
-    +'<p>These topics include, crime, meetups, announcements, pets, improvement, politics, housing, family, events, and kindness'
-    +'<p>For example you can ask Block Party, "What are people saying about crime in Chicago"'
+    +'<p>These topics include, crime, meetups, announcements, pets, improvements, politics, housing, family, events, and kindness</p>'
+    +'<p>For example you can ask Block Party, What are people saying about crime in Chicago, or, tell me about pets in Boston.</p>'
     );
   }
 );
